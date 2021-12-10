ea4a5798cc30ea5f0957286443e37148f79173bd
CleitonOliveira <cleiton.nso2019@gmail.com>
2021-12-10 11:22:34 -0300

    create component home-pokemon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,14 @@ import { AppComponent } from './app.component';
 import { LottieModule } from 'ngx-lottie';
 import player from 'lottie-web';
 import { AnimationLottieComponent } from './modules/animation-lottie/animation-lottie.component';
+import { HomePokemonComponent } from './modules/home-pokemon/home-pokemon.component';
 
 export function playerFactory() {
   return player;
 }
 
 @NgModule({
-  declarations: [AppComponent, AnimationLottieComponent],
+  declarations: [AppComponent, AnimationLottieComponent, HomePokemonComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
