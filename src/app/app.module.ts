5919127b01aef8328f9c71af09d41a9e5566d573
CleitonOliveira <cleiton.nso2019@gmail.com>
2021-12-10 12:04:21 -0300

    adicionando animacao lottie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { LottieModule } from 'ngx-lottie';
-import player from 'lottie-web';
-import { AnimationLottieComponent } from './modules/animation-lottie/animation-lottie.component';
-import { HomePokemonComponent } from './modules/home-pokemon/home-pokemon.component';
-
-export function playerFactory() {
-  return player;
-}
-
 @NgModule({
-  declarations: [AppComponent, AnimationLottieComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    LottieModule.forRoot({ player: playerFactory }),
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/modules/home-pokemon/home-pokemon.module.ts
+++ src/app/modules/home-pokemon/home-pokemon.module.ts
@@ -1,12 +1,24 @@
-import { HomePokemonComponent } from './home-pokemon.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomePokemonRoutingModule } from './home-pokemon-routing.module';
 
-const COMPONENTS = [HomePokemonComponent];
+import { HomePokemonComponent } from './home-pokemon.component';
+import { HomePokemonRoutingModule } from './home-pokemon-routing.module';
+import { AnimationLottieComponent } from '../animation-lottie/animation-lottie.component';
+import { LottieModule } from 'ngx-lottie';
+import player from 'lottie-web';
+
+export function playerFactory() {
+  return player;
+}
+
+const COMPONENTS = [HomePokemonComponent, AnimationLottieComponent];
 
 @NgModule({
   declarations: [COMPONENTS],
-  imports: [CommonModule, HomePokemonRoutingModule],
+  imports: [
+    CommonModule,
+    HomePokemonRoutingModule,
+    LottieModule.forRoot({ player: playerFactory }),
+  ],
 })
 export class HomePokemonModule {}
